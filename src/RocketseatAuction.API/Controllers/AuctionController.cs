4044660d0da2a4c36b0a080b3dec9e32aec1e7b9
Everton Oliveira <tonn.oliveira2011@gmail.com>
2024-02-06 18:36:36 -0300

    feat: Aula 01

---
+++ src/RocketseatAuction.API/Controllers/AuctionController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RocketseatAuction.API.Entities;
+using RocketseatAuction.API.UseCases.Auctions.GetCurrent;
+
+namespace RocketseatAuction.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuctionController : ControllerBase
+    {
+        [HttpGet]
+        [ProducesResponseType(typeof(Auction),StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        public IActionResult GetCurrentAuction()
+        {
+            var useCase = new GetCurrentAuctionUseCase();
+            var result = useCase.Execute();
+
+            if(result != null)
+            {
+                return NoContent();
+            }
+
+            return Ok(result);
+        }
+    }
+}
---
+++ src/RocketseatAuction.API/Repositories/RocketseatAuctionDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RocketseatAuction.API.Entities;
+
+namespace RocketseatAuction.API.Repositories
+{
+    public class RocketseatAuctionDbContext: DbContext
+   
+    {
+        public DbSet<Auction> Auctions { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source=C:\Users\everton.oliveira\Desktop\leilaoDbNLW.db");
+        }
+    }
+}
